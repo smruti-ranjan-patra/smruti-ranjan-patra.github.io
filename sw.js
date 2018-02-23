f691b2bf2ca02bd5b312ce8fe96e19cfd25590ab
Smruti Ranjan <smruti94srp@gmail.com>
2018-02-23 19:59:10 +0530

    code commit

--- sw.js
+++ sw.js
@@ -20,8 +20,45 @@ self.addEventListener('install', function(event){
 	console.log(event.request);
 });*/
 
-self.addEventListener('fetch', function(event){
+/*self.addEventListener('fetch', function(event){
 	// console.log('Fetch event request :- ');
 	// console.log(event.request);
 	console.log(777);
+});*/
+
+self.addEventListener('fetch', function (e) {
+    console.log("[ServiceWorker] Fetching", e.request.url);
+    const excludeList = excludeUrls.map(function (route) {
+        return e.request.url.includes(route);
+    });
+    if (!excludeList.includes(true)) {
+        e.respondWith(
+            caches.match(e.request).then(function (response) {
+                if (response) {
+                    console.log("[Service Worker] Found in cache", e.request.url)
+                    return response;
+                }
+
+                const requestClone = e.request.clone();
+                fetch(requestClone)
+                    .then(function (response) {
+                        if (!response) {
+                            console.log("[ServiceWorker] No response from fetch");
+                            return response;
+                        }
+
+                        const responseClone = response.clone();
+                        caches.open(cacheName).then(function (cache) {
+                            cache.put(e.request, responseClone);
+                            return response;
+                        });
+                    })
+                    .catch(function (error) {
+                        console.log("[ServiceWorker] Error Fetching && caching new data");
+                    })
+            })
+        )
+    }
+
+    console.log(777);
 });
