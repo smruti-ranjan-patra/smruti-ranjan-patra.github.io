e66fa1458a5c36dd394e1d1dca06acd3b59287ec
Smruti Ranjan <smruti94srp@gmail.com>
2018-02-23 13:39:37 +0530

    code commit

--- sw.js
+++ sw.js
@@ -13,6 +13,16 @@ if ('serviceWorker' in navigator) {
 }
 
 
+self.addEventListener('install', function(event){
+	console.log('Install event request :- ');
+	console.log(event.request);
+});
+
+self.addEventListener('activate', function(event){
+	console.log('Activate event request :- ');
+	console.log(event.request);
+});
+
 self.addEventListener('fetch', function(event){
 	console.log('Fetch event request :- ');
 	console.log(event.request);
