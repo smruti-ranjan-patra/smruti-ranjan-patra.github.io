ed0f362452446b0913bc4e21305aa0752a28187f
Smruti Ranjan <smruti94srp@gmail.com>
2018-02-23 15:22:54 +0530

    code commit

--- sw.js
+++ sw.js
@@ -1,18 +1,3 @@
-
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-
 self.addEventListener('install', function(event){
 	console.log('Install event request :- ');
 	console.log(event.request);
