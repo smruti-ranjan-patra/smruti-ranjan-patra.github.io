6c642d8d19aa50c83763764459f77c54b1d1c475
Smruti Ranjan <smruti94srp@gmail.com>
2018-02-23 16:24:31 +0530

    code commit

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-self.addEventListener('install', function(event){
-	// console.log('Install event request :- ');
+/*self.addEventListener('install', function(event){
+	console.log('Install event request :- ');
+	console.log(event.request);
+});
+
+self.addEventListener('activate', function(event){
+	console.log('Activate event request :- ');
+	console.log(event.request);
+});*/
+
+self.addEventListener('fetch', function(event){
+	// console.log('Fetch event request :- ');
 	// console.log(event.request);
 	console.log(123);
 });
-
-/*self.addEventListener('activate', function(event){
-	console.log('Activate event request :- ');
-	console.log(event.request);
-});
-
-self.addEventListener('fetch', function(event){
-	console.log('Fetch event request :- ');
-	console.log(event.request);
-});*/
