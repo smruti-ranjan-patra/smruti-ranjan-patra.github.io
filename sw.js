a6dc163ea3071270b87fa691d19c139ef5891fd8
Smruti Ranjan <smruti94srp@gmail.com>
2018-02-23 18:24:31 +0530

    code commit

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', function(event){
 	// Perform install steps
 	event.waitUntil(
 	caches.open(CACHE_NAME)
-		then(function(cache) {
+		.then(function(cache) {
 			console.log('Opened cache');
 		return cache.addAll(urlsToCache);
 		})
