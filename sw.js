3f8a484235d75a4f17150e9edadebc2289dd0020
Smruti Ranjan <smruti94srp@gmail.com>
2018-02-26 13:34:50 +0530

    code commited

--- sw.js
+++ sw.js
@@ -10,15 +10,26 @@ self.addEventListener('install', function(e){
     console.log("[ServiceWorker] Installed");
 
     e.waitUntil(
-    		caches.open(cacheName).then(function(cache) {
-    			console.log("[ServiceWorker] Caching cacheFiles");
-    			return cache.addAll(cacheFiles);
-    		})
-    	)
+		caches.open(cacheName).then(function(cache) {
+			console.log("[ServiceWorker] Caching cacheFiles");
+			return cache.addAll(cacheFiles);
+		})
+	)
 });
 
 self.addEventListener('activate', function(e){
     console.log("[ServiceWorker] Acticated");
+
+    e.waitUntil(
+    	caches.keys().then(function(cacheNames) {
+			return Promise.all(cacheNames.map(function(thisCacheName) {
+				if (thisCacheName !== cacheName) {
+					console.log("[ServiceWorker] Removing Cached Files from ", thisCacheName);
+					return caches.delete(thisCacheName);
+				}
+			}))
+		})
+	)
 });
 
 self.addEventListener('fetch', function(e){
